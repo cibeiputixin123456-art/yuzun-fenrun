621fe429553c7f907382f6f11b4ab434af40a75f
yuzun <yuzun@example.com>
2026-06-03 16:40:30 +0800

    fix: no eval build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    target: 'es2015',
+    cssCodeSplit: false,
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
